c38fb12d53e97b4754e3548f441803be05fdaa63
Link4Electronics <link4electronics@gmail.com>
2026-08-15 19:52:52 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q neochat | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q neochat | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -22,7 +22,6 @@ quick-sharun /usr/bin/neochat \
   /usr/lib/libKirigami*.so* \
   /usr/share/icons/hicolor/scalable/apps/org.kde.neochat.tray.svg
 
-# Additional changes can be done in between here
 echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
