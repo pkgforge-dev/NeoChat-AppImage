39699f992f72a486c9749982e11579e6b20d63ff
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:08:02 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,8 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.kde.neochat.svg
 export DESKTOP=/usr/share/applications/org.kde.neochat.desktop
 export STARTUPWMCLASS=org.kde.neochat
-export DEPLOY_QT=1
-export QT_DIR=qt6
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 export DEPLOY_PULSE=1
 
 # Deploy dependencies
